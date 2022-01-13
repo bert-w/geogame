9919c63188b269a8bd31c538f45d175d2cad7759
merkelmauer <markopetkovic1999@gmail.com>
2022-01-13 12:55:53 +0100

    Initial Code to transform polygon to their polygon and calculate union

--- Assets/Scripts/PlaceLightsController.cs
+++ Assets/Scripts/PlaceLightsController.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using Util.Geometry.Polygon;
+using Util.Algorithms.Polygon;
 
 public class PlaceLightsController : MonoBehaviour
 {
@@ -15,24 +17,26 @@ public class PlaceLightsController : MonoBehaviour
     private List<PolygonVertex> eventQueue;
 
     public Polygon visibilityPolygon;
-    
+
     public float LineWidth;
 
     public Color LineColor = Color.black;
 
     // List with "confirmed" visibility polygons
-    public List<Polygon> visibilityPolygonList; 
+    public List<Polygon> visibilityPolygonList;
     // List with polygons to visualize, I.e. while still moving the light
-    public List<Polygon> drawVisibilityPolygonList; 
+    public List<Polygon> drawVisibilityPolygonList;
 
     private Polygon challengePolygon;
 
     private LineRenderer visibilityPolygonLine;
 
+    private UnionSweepLine unionSweepLine = new UnionSweepLine();
+
 
     // Start is called before the first frame update
     void Start()
-    {    
+    {
         mouseLight = Instantiate(mouseLight, transform);
         mouseLight.name = "Mouse Light";
         mouseLight.GetComponent<SpriteRenderer>().color = Color.yellow;
@@ -50,9 +54,32 @@ public class PlaceLightsController : MonoBehaviour
     {
         mouseLight.transform.position = GetMousePosition();
 
-        // if (Input.GetButtonDown("Fire1")){
-            GenerateVisibilityPolygon();
-        // }
+        GenerateVisibilityPolygon();
+        if (Input.GetButtonDown("Fire1")){
+            visibilityPolygonList.Add(CreateNewVisibilityPolygon());
+            visibilityPolygonList = MergeVisibilityPolygons(visibilityPolygonList);
+        }
+    }
+
+    Polygon ChangePol2DToPol(Polygon2D pol2d)
+    {
+        Polygon pol = new Polygon();
+        foreach (var item in pol2d.Vertices)
+        {
+            pol.vertices.Add(new PolygonVertex { x = item.x, y = item.y });
+        }
+        return pol;
+    }
+
+    Polygon2D ChangePolToPol2D(Polygon pol)
+    {
+        var vectorList = new List<Vector2>();
+        foreach (var vert in pol.vertices)
+        {
+            vectorList.Add(vert.ToVector());
+        }
+        IEnumerable<Vector2> collection = vectorList;
+        return new Polygon2D(collection);
     }
 
     void GenerateVisibilityPolygon()
@@ -60,14 +87,14 @@ public class PlaceLightsController : MonoBehaviour
         Vector3 mPos = GetMousePosition();
 
         visibilityPolygon.empty();
-        
+
         var visibilityPolygonEdges = GenerateVisibilityPolygon(mPos);
 
         // Goed checken
         foreach (var edge in visibilityPolygonEdges)
         {
             // TODO de punten staan in order can visibility dus voor het teken moet je alleen kijken wel punt overlap en op basis daarvan start en end swappen
-            visibilityPolygon.Add(edge.start); 
+            visibilityPolygon.Add(edge.start);
             visibilityPolygon.Add(edge.end);
         }
         visibilityPolygon.Add(visibilityPolygonEdges.First().start);
@@ -89,6 +116,24 @@ public class PlaceLightsController : MonoBehaviour
         visibilityPolygon.empty();
     }
 
+
+    // @TODO update this with latest code
+    Polygon CreateNewVisibilityPolygon()
+    {
+        Polygon newVis = new Polygon();
+        Vector3 mPos = GetMousePosition();
+
+        var visibilityPolygonEdges = GenerateVisibilityPolygon(mPos);
+        foreach (var edge in visibilityPolygonEdges)
+        {
+            // TODO de punten staan in order can visibility dus voor het teken moet je alleen kijken wel punt overlap en op basis daarvan start en end swappen
+            newVis.Add(edge.start);
+            newVis.Add(edge.end);
+        }
+        newVis.Add(visibilityPolygonEdges.First().start);
+
+        return newVis;
+    }
     // Generate an event queue (radial sweep) for the visibility polygon from point mPos.
     private List<Event> GenerateEventQueue(Vector3 mPos)
     {
@@ -150,10 +195,10 @@ public class PlaceLightsController : MonoBehaviour
         var queue = GenerateEventQueue(mPos);
         var state = new RedBlackTree<Event>();
 
-        for (int i = 0; i < queue.Count; i+=2)
+        for (int i = 0; i < queue.Count; i += 2)
         {
             var event1 = queue[i];
-            var event2 = queue[i+1];
+            var event2 = queue[i + 1];
 
             if (event1.Type == EventType.Start && event2.Type == EventType.Start)
             {
@@ -208,127 +253,44 @@ public class PlaceLightsController : MonoBehaviour
     // merges currently existing visibility polygons with the latest polygon
     // can be used both for updating the polygons to be drawn and 
     // placing a new light
-    public void MergeVisibilityPolygons(List<Polygon> visibilityPolygonList, Polygon newVisibility)
+    public List<Polygon> MergeVisibilityPolygons(List<Polygon> visibilityPolygonList)
     {
-        // all polygons in visibilityPolygonList do not intersect
-        // create list where we store all polygons which intersect with new visibility
-        // iteratively union the latest polygon with the old polygons
-        // finally, return new polygon visibility list
-        var tempPolygonList = new List<Polygon>();
-        
-    }
 
-    public void MergeTwoPolygons(Polygon polygon1, Polygon polygon2, List<(PolygonVertex, Edge)> links)
-    {
-        // Find point with highest x
-        PolygonVertex highest1 = polygon1.PolygonYMax();
-        PolygonVertex highest2 = polygon2.PolygonYMax();
-        PolygonVertex highestVertex = (highest1.y > highest2.y) ? highest1 : highest2;
-        PolygonVertex currVertex = highestVertex;
-        Edge prevEdge = polygon1.edges[1]; // PLACEHOLDER EDGE
-        // @TODO start new polygon
-        // @TODO "move" over the edge starting at the highest point
-        // @TODO something to ensure we are moving in the right direction
-        while (!currVertex.IsEqual(highestVertex))
+        ICollection<Polygon2D> pol2dCol = new List<Polygon2D>();
+        List<Polygon> newVisPoly = new List<Polygon>();
+        foreach (var item in visibilityPolygonList)
         {
-            var maxAngle = CalcAngleEdges(prevEdge, currVertex.nextEdge);
-            var maxEdge = prevEdge;
-            foreach (var item in links)
-            {
-                if ((item.Item1.IsEqual(currVertex))){
-                    var currAngle = CalcAngleEdges(prevEdge, item.Item2);
-                    if (currAngle > maxAngle){
-                        maxAngle = currAngle;
-                        maxEdge = item.Item2;
-                    }
-                }
-            }
-
-            // @TODO add maxEdge to polygon
-
-        }
-        
-        // @TODO return polygon
-
-    }
-
-    public float CalcAngleEdges(Edge edge1, Edge edge2)
-    {
-        return 10.0f;
-    }
-
-
-    // calculates the intersections between 2 visibility polygons
-    // places new vertices on segments with intersections
-    // we naively check intersections between each segment
-    // since segments might overlap, we need special cases for overlapiing segments
-    // returns a list with vertices and which edges from different polygons they are linked to
-    // @TODO implement as sweepline?
-    // @TODO move part of the code to edge class?
-    // @TODO make dictionary?
-    public List<(PolygonVertex, Edge)> CalculateIntersections(Polygon polygon1, Polygon polygon2)
-    {
-        List <(PolygonVertex, Edge)> newLinks = new List<(PolygonVertex, Edge)>();
-
-        for (int i = 0; i < polygon1.edges.Count; i++)
-        {
-            Edge edge1 = polygon1.edges[i];
-            for (int j = 0; j < polygon2.edges.Count; j++)
-            {
-                Edge edge2 = polygon2.edges[j];
-                var crossingPoint = edge1.Crosses(edge2);
-                // Case 1: both edges are the same edge
-                if (edge1.IsEqual(edge2))
-                {
-                    // if the edges are "reversed"
-                    if (edge1.IsReversed(edge2)) {
-                        // start vertex of edge 1 is end vertex of edge 2
-                        newLinks.Add((edge1.startVertex, edge2));
-                        newLinks.Add((edge1.startVertex, edge2.endVertex.nextEdge));
-                        newLinks.Add((edge1.endVertex, edge2));
-                        newLinks.Add((edge1.endVertex, edge2.startVertex.prevEdge));
-
-                        newLinks.Add((edge2.startVertex, edge1));
-                        newLinks.Add((edge2.startVertex, edge1.endVertex.nextEdge));
-                        newLinks.Add((edge2.endVertex, edge1));
-                        newLinks.Add((edge2.endVertex, edge1.startVertex.prevEdge));
-
-                    }
-                    // otherwise
-                    else
-                    {
-                        newLinks.Add((edge1.startVertex, edge2));
-                        newLinks.Add((edge1.startVertex, edge2.startVertex.prevEdge));
-                        newLinks.Add((edge1.endVertex, edge2));
-                        newLinks.Add((edge1.endVertex, edge2.endVertex.nextEdge));
-
-                        newLinks.Add((edge2.startVertex, edge1));
-                        newLinks.Add((edge2.startVertex, edge1.startVertex.prevEdge));
-                        newLinks.Add((edge2.endVertex, edge1));
-                        newLinks.Add((edge2.endVertex, edge1.endVertex.prevEdge));
-
-                    }
-                }
-
-                // Case 2: Edges overlap
-                else if (edge1.Overlaps(edge2))
-                {
-                    // @TODO calculate "intersections"
-                    // @TODO insert new edges and vertices in both polygons
-                    // @TODO create "jumping points" between polygons and add them to newlinks
-                }
-
-                // Case 3: Edges intersect
-                else if (null != crossingPoint)
-                {
-                    // @TODO insert vertex for both polygons
-                    // @TODO split edge in two edges for both polygons
-                    // @TODO create "jumping points" between polygons and add them to newlinks
-                }
-            }
+            pol2dCol.Add(ChangePolToPol2D(item));
         }
 
-        return newLinks;
+        var mergedVisibilityPolygon = unionSweepLine.Union(pol2dCol);
+
+        if (mergedVisibilityPolygon.GetType() == typeof(MultiPolygon2D)){
+            MultiPolygon2D mul = (MultiPolygon2D)mergedVisibilityPolygon;
+            foreach (var item in mul.Polygons)
+            {
+                newVisPoly.Add(ChangePol2DToPol(item));
+            }
+            
+        }
+        else if (mergedVisibilityPolygon.GetType() == typeof(Polygon2D))
+        {
+            Polygon2D pol2d = (Polygon2D)mergedVisibilityPolygon;
+            newVisPoly.Add(ChangePol2DToPol(pol2d));
+        }
+        else
+        {
+            Debug.Log("Failed");
+            Debug.Log(mergedVisibilityPolygon.GetType());
+        }
+
+        return newVisPoly;
+
+
 
     }
+
 }
+
+
+
